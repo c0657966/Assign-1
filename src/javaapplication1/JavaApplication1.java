89e37fdb6c67e7998e67183160f0602ed165bfce
sahil <sahil@10.55.68.13>
2015-09-16 12:38:29 -0400

    Assign 1

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author sahil
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*  Job Posting
+                
+      Job position - Senior Software Engineer
+        
+        Technologies required:
+        
+        MVC framework - https://en.wikipedia.org/wiki/Model%E2%80%93view%E2%80%93controller
+        Linux - https://en.wikipedia.org/wiki/Linux
+        Hibernate - https://en.wikipedia.org/wiki/Hibernate_(Java)
+        Spring core module - https://en.wikipedia.org/wiki/Spring_Framework
+        liferay - https://en.wikipedia.org/wiki/Liferay
+        Micro Service Development - https://en.wikipedia.org/wiki/Microservices
+        Java - https://en.wikipedia.org/wiki/Java_(software_platform)
+        Web Services REST, SOAP - https://en.wikipedia.org/wiki/Web_service
+        SQL - https://en.wikipedia.org/wiki/SQL
+                
+          Potential Salary:
+                
+                Toronto - $93k
+                Vancouver - $99k
+                Waterloo - $86k
+          */
+                
+                }
+    
+}
